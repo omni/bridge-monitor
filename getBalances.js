9fee6e258fc966db8b0d0c90c4c386d5e38875ca
Roman Storm <rstormsf@gmail.com>
2018-04-16 20:27:48 -0700

    add bn.js

--- getBalances.js
+++ getBalances.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const fs = require('fs')
 const Web3 = require('web3');
 const Web3Utils = require('web3-utils')
+const BN = require('bignumber.js')
 const HOME_RPC_URL = process.env.HOME_RPC_URL;
 const FOREIGN_RPC_URL = process.env.FOREIGN_RPC_URL;
 const HOME_BRIDGE_ADDRESS = process.env.HOME_BRIDGE_ADDRESS;
@@ -21,9 +22,9 @@ async function main(){
     const homeBalance = await web3Home.eth.getBalance(HOME_BRIDGE_ADDRESS)
     const tokenContract = new web3Foreign.eth.Contract(ERC677_ABI, POA20_ADDRESS);
     const totalSupply = await tokenContract.methods.totalSupply().call()
-    const homeBalanceBN = new Web3Utils.BN(homeBalance)
-    const foreignTotalSupplyBN = new Web3Utils.BN(totalSupply)
-    const diff = homeBalanceBN.sub(foreignTotalSupplyBN).toString(10)
+    const homeBalanceBN = new BN(homeBalance)
+    const foreignTotalSupplyBN = new BN(totalSupply)
+    const diff = homeBalanceBN.minus(foreignTotalSupplyBN).toString(10)
     return {
       homeBalance: Web3Utils.fromWei(homeBalance),
       foreignTotalSupply: Web3Utils.fromWei(totalSupply),
