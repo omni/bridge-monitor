5dfe98cf2789631d395f95a7e15e063a9655ac80
Roman Storm <rstormsf@gmail.com>
2018-06-13 18:52:16 -0700

    add eventStats

--- eventsStats.js
+++ eventsStats.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
-const fs = require('fs')
 const Web3 = require('web3');
-const Web3Utils = require('web3-utils')
 const HOME_RPC_URL = process.env.HOME_RPC_URL;
 const FOREIGN_RPC_URL = process.env.FOREIGN_RPC_URL;
 const HOME_BRIDGE_ADDRESS = process.env.HOME_BRIDGE_ADDRESS;
 const FOREIGN_BRIDGE_ADDRESS = process.env.FOREIGN_BRIDGE_ADDRESS;
-const POA20_ADDRESS = process.env.POA20_ADDRESS;
 
 const homeProvider = new Web3.providers.HttpProvider(HOME_RPC_URL);
 const web3Home = new Web3(homeProvider);
@@ -14,10 +11,8 @@ const web3Home = new Web3(homeProvider);
 const foreignProvider = new Web3.providers.HttpProvider(FOREIGN_RPC_URL);
 const web3Foreign = new Web3(foreignProvider);
 
-const HOME_ABI = require('./Home_bridge.abi');
-const FOREIGN_ABI = require('./Foreign.abi')
-
-const db = require('./db.json')
+const HOME_ABI = require('./abis/Home_bridge.abi');
+const FOREIGN_ABI = require('./abis/Foreign.abi')
 
 function compareDepositsHome(foreign){
   return function(homeDeposit){
@@ -41,57 +36,31 @@ function compareDepositsForeign(home){
 async function main(){
   try {
 
+    const FROM_BLOCK = 0;
+
     const homeBridge = new web3Home.eth.Contract(HOME_ABI, HOME_BRIDGE_ADDRESS);
     const foreignBridge = new web3Foreign.eth.Contract(FOREIGN_ABI, FOREIGN_BRIDGE_ADDRESS);
-    let homeDeposits = await homeBridge.getPastEvents('Deposit', {fromBlock: db.home.processedBlock});
-    let foreignDeposits = await foreignBridge.getPastEvents('Deposit', {fromBlock: db.foreign.processedBlock});
-    let homeWithdrawals = await homeBridge.getPastEvents('Withdraw', {fromBlock: db.home.processedBlock});
-    let foreignWithdrawals = await foreignBridge.getPastEvents('Withdraw', {fromBlock: db.foreign.processedBlock});
+    let homeDeposits = await homeBridge.getPastEvents('Deposit', {fromBlock: FROM_BLOCK});
+    let foreignDeposits = await foreignBridge.getPastEvents('Deposit', {fromBlock: FROM_BLOCK});
+    let homeWithdrawals = await homeBridge.getPastEvents('Withdraw', {fromBlock: FROM_BLOCK});
+    let foreignWithdrawals = await foreignBridge.getPastEvents('Withdraw', {fromBlock: FROM_BLOCK});
     
-    const onlyInHomeDeposits = homeDeposits.concat(db.home.onlyInHomeDeposits).filter(compareDepositsHome(foreignDeposits))
-    const onlyInForeignDeposits = foreignDeposits.concat(db.foreign.onlyInForeignDeposits).filter(compareDepositsForeign(homeDeposits))
+    const onlyInHomeDeposits = homeDeposits.filter(compareDepositsHome(foreignDeposits))
+    const onlyInForeignDeposits = foreignDeposits.concat([]).filter(compareDepositsForeign(homeDeposits))
 
     const onlyInHomeWithdrawals = homeWithdrawals.filter(compareDepositsForeign(foreignWithdrawals))
     const onlyInForeignWithdrawals = foreignWithdrawals.filter(compareDepositsHome(homeWithdrawals))
-    if(foreignWithdrawals.length > 0 || foreignDeposits > 0){
-      let withdrawalBlk = foreignWithdrawals[foreignWithdrawals.length - 1] ? foreignWithdrawals[foreignWithdrawals.length - 1].blockNumber : 0
-      let depositBlk = foreignDeposits[foreignDeposits.length - 1] ? foreignDeposits[foreignDeposits.length - 1].blockNumber : 0
-      db.foreign.processedBlock = Math.max(withdrawalBlk, depositBlk) + 1
-    }
-    if(homeDeposits.length > 0 || homeWithdrawals.length > 0){
-      let withdrawalBlk = homeWithdrawals[homeWithdrawals.length - 1] ? homeWithdrawals[homeWithdrawals.length - 1].blockNumber : 0
-      let depositBlk = homeDeposits[homeDeposits.length - 1]? homeDeposits[homeDeposits.length - 1].blockNumber : 0
-      db.home.processedBlock = Math.max(withdrawalBlk, depositBlk) + 1
-    }
-
-    if(onlyInHomeDeposits.length > 0 ){
-      db.home.onlyInHomeDeposits = db.home.onlyInHomeDeposits.concat(onlyInHomeDeposits)
-    }
-    if(onlyInHomeWithdrawals.length > 0 ){
-      db.home.onlyInHomeWithdrawals = db.home.onlyInHomeWithdrawals.concat(onlyInHomeWithdrawals)
-    }
-    if(onlyInForeignDeposits.length > 0) {
-      db.foreign.onlyInForeignDeposits = db.foreign.onlyInForeignDeposits.concat(onlyInForeignDeposits)
-    }
-    if(onlyInForeignWithdrawals.length > 0) {
-      db.foreign.onlyInForeignWithdrawals = db.foreign.onlyInForeignWithdrawals.concat(onlyInForeignWithdrawals)
-    }
-    db.foreign.deposits += foreignDeposits.length;
-    db.home.withdrawals += homeWithdrawals.length;
-    db.home.deposits += homeDeposits.length;
-    db.foreign.withdrawals += foreignWithdrawals.length;
     
-    fs.writeFileSync(__dirname + '/db.json', JSON.stringify(db,null,4));
     return {
-      ...db,
-      depositsDiff: db.home.deposits - db.foreign.deposits,
-      withdrawalDiff: db.home.withdrawals - db.foreign.withdrawals,
-      totalUnrelayedDeposits: db.home.onlyInHomeDeposits.length + db.foreign.onlyInForeignDeposits.length,
-      totalUnrelayedWithdrawals: db.home.onlyInHomeWithdrawals.length + db.foreign.onlyInForeignWithdrawals.length
+      onlyInHomeDeposits,
+      onlyInForeignDeposits,
+      onlyInHomeWithdrawals,
+      onlyInForeignWithdrawals
     }
   } catch(e) {
     console.error(e);
   }
 
 }
+main()
 module.exports = main;
