717b3e7bf7aad6607677fa94fb3ff6b503a4820f
Roman Storm <roman@poa.net>
2018-08-23 14:17:29 -0700

    Feature: Add stuckTransfers>

---
+++ checkWorker3.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const logger = require('./logger')('checkWorker3');
+const stuckTransfers = require('./stuckTransfers')
+
+async function checkWorker3() {
+  try {
+    logger.debug('calling stuckTransfers()');
+    const transfers = await stuckTransfers();
+    console.log(transfers)
+    if (!transfers) throw new Error('transfers is empty: ' + JSON.stringify(transfers));
+    fs.writeFileSync(__dirname + '/responses/stuckTransfers.json', JSON.stringify(transfers,null,4));
+    logger.debug("Done");
+    return transfers;
+  } catch(e) {
+    logger.error('checkWorker3.js', e);
+    throw e;
+  }
+}
+checkWorker3();
